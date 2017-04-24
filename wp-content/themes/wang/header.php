fa7ddebd2670e2522d8ae4399bf175eed4395ec3
renatto.rps <renatto.rps@gmail.com>
2017-04-24 15:30:49 -0300

    fazendo o tema aparecer no menu do wordpress

---
+++ wp-content/themes/wang/header.php
@@ -0,0 +1,12 @@
+<?php
+//  @header.php
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
---
+++ wp-content/themes/wang/index.php
@@ -0,0 +1,5 @@
+<?php
+//  @index.php
+
+// chama o header.php
+	get_header();
