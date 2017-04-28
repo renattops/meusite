7faaf80d4768877550958dd4a77f3473c118892c
renatto.rps <renatto.rps@gmail.com>
2017-04-27 23:09:10 -0300

    adicionando função de Background personalizavel ao functions.php

---
+++ wp-content/themes/wang/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// Background personalizavel
+$default_background = array(
+  'default-color'          => 'fff',
+  //'default-image'          => get_template_directory_uri() . '/images/',
+  //'default-repeat'         => 'no-repeat',
+  //'default-position-x'     => 'center',
+  //'default-attachment'     => 'fixed',
+);
+add_theme_support( 'custom-background', $default_background );
+// ***************
